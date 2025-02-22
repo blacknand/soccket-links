a80b37551e39714b93068091f80c59db02dbcbd9
blacknand <nblackburndeveloper@icloud.com>
2025-02-22 15:50:57 +0000

    init

---
+++ src/socket.c
@@ -0,0 +1,13 @@
+#include "socket.h"
+
+#include <netinet/in.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <unistd.h>
+
+int main() {
+	printf("Hello world\n");
+	return 0;
+}
---
+++ src/socket.h
@@ -0,0 +1,8 @@
+#ifndef SOCKET_H
+#define SOCKET_H
+
+#pragma once
+
+#define PORT 8080
+
+#endif	// SOCKET_H
